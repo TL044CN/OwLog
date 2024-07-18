643cdb720918d4b701bb52d250017979469f483e
TL044CN <gunnisan2@web.de>
2024-07-18 21:15:30 +0200

    ADD: Logger Tests

---
+++ tests/test_main.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_RUNNER
+#include "catch2/catch_session.hpp"
+
+int main(int argc, char* argv[]) {
+    Catch::Session session;
+    int returnCode = session.applyCommandLine(argc, argv);
+    if (returnCode != 0) {
+        return returnCode;
+    }
+    return session.run(argc, argv);
+}
